8c3dc9c0115841e2e1044e2b6b5124a29845cbc7
Charlie B <cbutcosk@gmail.com>
2023-03-15 09:57:09 -0400

    First serveable POC -- signal loading / done, spit JSON on the page

--- static/index.js
+++ static/index.js
@@ -1,24 +1,35 @@
-async function run() {
-    let pyodide = await loadPyodide()
+document.addEventListener("DOMContentLoaded",() => {
 
-    await pyodide.loadPackage("micropip")
-    micropip = pyodide.pyimport("micropip")
-    await micropip.install("./cromulent-0.16.11-py3-none-any.whl")
-    return pyodide.runPythonAsync(`
-    from cromulent.model import factory
-    from cromulent.vocab import Painting, PrimaryName
+    async function run() {
+        let pyodide = await loadPyodide()
 
-    pt = Painting(ident="object/1")
-    nm = PrimaryName(content="A Nice Painting")
+        await pyodide.loadPackage("micropip")
+        micropip = pyodide.pyimport("micropip")
+        await micropip.install("./dist/cromulent-0.16.11-py3-none-any.whl")
+        return pyodide.runPythonAsync(`
+        import json
+        from cromulent.model import factory
+        from cromulent.vocab import Painting, PrimaryName
 
-    pt.identified_by = nm
+        pt = Painting(ident="object/1")
+        nm = PrimaryName(content="A Nice Painting")
 
-    factory.toString(pt)
-    `);
-}
+        pt.identified_by = nm
 
-run().then((result) => {
-    console.debug("Running python")
-    console.log(result)
-    console.debug("Done running python")
-});
+        data = factory.toJSON(pt)
+        json.dumps(data,indent=2)
+        `);
+    }
+
+    run().then((result) => {
+        let headerEl = document.getElementById("py-env-status")
+        headerEl.innerHTML = "linked.art JSON-LD generated in-browser"
+
+        let el = document.getElementById("py-results-code")
+        el.innerHTML = result
+        // console.debug("Running python")
+        console.log(typeof result)
+        console.debug("Done running python")
+    });
+
+})
